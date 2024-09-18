6e0581331ed7e65584958198045b0fc3a902bce2
divijghose <divijghose@iisc.ac.in>
2024-09-18 19:54:46 +0530

    Parallel runs with optuna tuner 1. Creates an SQLite database if it doesn't exist. Can be used for stalled runs or parallel implementation. 2. Lists available number of GPUs and divides jobs.

--- fastvpinns/hyperparameter_tuning/optuna_tuner.py
+++ fastvpinns/hyperparameter_tuning/optuna_tuner.py
@@ -20,22 +20,42 @@ Note:
 
 import optuna
 from .objective import objective
-
+import tensorflow as tf
+import os
 
 class OptunaTuner:
-    def __init__(self, n_trials=100, study_name="fastvpinns_optimization"):
+    def __init__(self, n_trials=100, study_name="fastvpinns_optimization", n_jobs=-1, n_epochs=5000):
         self.n_trials = n_trials
         self.study_name = study_name
+        self.n_jobs = n_jobs
+        self.n_epochs = n_epochs
+        self.gpus = tf.config.list_physical_devices('GPU')
+        print(f"Available GPUs: {len(self.gpus)}")
+
+    def objective_wrapper(self, trial):
+        """
+        Wrapper function to run the objective function on a specific GPU.
+        """
+
+        gpu_id = trial.number % len(self.gpus)
+        with tf.device(f'/device:GPU:{gpu_id}'):
+            return objective(trial, self.n_epochs)
+
 
     def run(self):
-        study = optuna.create_study(direction="minimize", study_name=self.study_name)
-        study.optimize(objective, n_trials=self.n_trials)
+        study = optuna.create_study(
+            study_name=self.study_name,
+            direction="minimize",
+            storage="sqlite:///fastvpinns_optuna.db",
+            load_if_exists=True
+        )
+        study.optimize(self.objective_wrapper, n_trials=self.n_trials, n_jobs=min(len(self.gpus), self.n_jobs))
 
         print("Best trial:")
         trial = study.best_trial
         print("  Value: ", trial.value)
         print("  Params: ")
         for key, value in trial.params.items():
-            print(f"    {key}: {value}")
+            print("    {}: {}".format(key, value))
 
         return study.best_params
