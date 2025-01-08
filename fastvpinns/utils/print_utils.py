07e78b289b1f132f614e178836a8fafd737fc5cf
divijghose <divijghose@iisc.ac.in>
2025-01-08 17:41:10 +0530

    Fixed bugs in print utility file

--- fastvpinns/utils/print_utils.py
+++ fastvpinns/utils/print_utils.py
@@ -43,8 +43,48 @@ def print_table(title, columns, col_1_values, col_2_values):
         if isinstance(val_2, float):
             # add the row to the table
             table.add_row(val_1, f"{val_2:.4f}")
+        elif isinstance(val_2, int):
+            table.add_row(val_1, f"{val_2}")
         else:
             table.add_row(val_1, str(val_2))
+
+    # Print the table to the console
+    console.print(table)
+
+
+def print_table_multicolumns(title, column_names, col_vals_list):
+    # Create a console object
+    console = Console()
+
+    # Create a table with a title
+    table = Table(show_header=True, header_style="bold magenta", title=title)
+
+    # check for the number of columns and the number of values
+    if len(column_names) != len(col_vals_list):
+        print(f"[ERROR] - The number of columns and the number of values are not equal")
+        exit(0)
+        return
+
+    # check length of every list in col_vals_list is equal to the length of the first list
+    for col_vals in col_vals_list:
+        if len(col_vals) != len(col_vals_list[0]):
+            print(f"[ERROR] - The length of the lists in col_vals_list are not equal")
+            exit(0)
+            return
+
+    # Add columns to the table
+    for column in column_names:
+        table.add_column(column)
+
+    # Add rows to the table
+    num_cols = len(column_names)
+
+    for i in range(len(col_vals_list[0])):
+        row_vals = []
+        for j in range(num_cols):
+            row_vals.append(str(col_vals_list[j][i]))
+
+        table.add_row(*row_vals)
 
     # Print the table to the console
     console.print(table)
