5620ccfd146b3d6bb91ed7348acdce3b21b7e248
divijghose <divijghose@iisc.ac.in>
2025-01-17 00:48:52 +0530

    feat: Base class for window functions with abstract methods defined

--- fastvpinns/domain_decomposition/window_functions/base.py
+++ fastvpinns/domain_decomposition/window_functions/base.py
@@ -11,7 +11,7 @@ class WindowFunction:
     Base class for window functions.
     """
 
-    def __init__(self, subdomain_mean_list, subdomain_span_list, kernel_type):
+    def __init__(self):
         """
         Initialize the window function.
 
@@ -22,29 +22,6 @@ class WindowFunction:
         subdomain_span_list : list
             List of the spans of the subdomains. First dimension is the x_coordinate, second dimension is the y_coordinate.
         """
-        self.subdomain_mean_list = subdomain_mean_list
-        self.subdomain_span_list = subdomain_span_list
-
-        self.x_mean_list = subdomain_mean_list[0]
-        self.y_mean_list = subdomain_mean_list[1]
-
-        self.x_span_list = subdomain_span_list[0]
-        self.y_span_list = subdomain_span_list[1]
-
-        self.kernel_type = kernel_type
-
-        if self.kernel_type not in ['cosine']:
-            raise ValueError('Kernel type not recognized.')
-
-    def get_min_max(self):
-        """
-        Get the minimum and maximum values of the subdomains.
-        """
-
-        self.x_min = [mean - span / 2 for mean, span in zip(self.x_mean_list, self.x_span_list)]
-        self.x_max = [mean + span / 2 for mean, span in zip(self.x_mean_list, self.x_span_list)]
-        self.y_min = [mean - span / 2 for mean, span in zip(self.y_mean_list, self.y_span_list)]
-        self.y_max = [mean + span / 2 for mean, span in zip(self.y_mean_list, self.y_span_list)]
 
     @abstractmethod
     def get_kernel(self):
