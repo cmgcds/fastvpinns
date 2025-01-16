39658d22376c9216dd3adfcdbb9f4a1e00bdec12
divijghose <divijghose@iisc.ac.in>
2025-01-17 00:49:35 +0530

    feat: Ner implementation of FE spaces specifically for domain decompositon

--- fastvpinns/FE/fespace_domain_decomposition.py
+++ fastvpinns/FE/fespace_domain_decomposition.py
@@ -8,8 +8,30 @@ class FESpaceDomainDecomposition(Fespace2D):
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, mesh=None, **kwargs)
+        super().__init__(
+            *args,
+            mesh=None,
+            bound_function_dict=None,
+            bound_condition_dict=None,
+            boundary_points=None,
+            **kwargs,
+        )
         assert (
             self.mesh is None
         ), 'FESpaceDomainDecomposition does not support external meshes. domain.mesh should be None.'
+        assert (
+            self.bound_function_dict is None
+        ), 'FESpaceDomainDecomposition does not support soft boundary constraints. domain.bound_function_dict should be None.'
+        assert (
+            self.bound_condition_dict is None
+        ), 'FESpaceDomainDecomposition does not support hard boundary constraints. domain.bound_condition_dict should be None.'
+        assert (
+            self.boundary_points is None
+        ), 'FESpaceDomainDecomposition does not support boundary point sampling, hard constraints are used instead. domain.boundary_points should be None.'
+        pass
+
+    def generate_dirichlet_boundary_data(self):
+        pass
+
+    def generate_dirichlet_boundary_data_vector(self, component):
         pass
